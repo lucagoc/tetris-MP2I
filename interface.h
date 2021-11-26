87aa00abc7a93048af00c6df508136d254daa566
lucagoc <lucas07360@gmail.com>
2021-11-26 20:46:08 +0100

    ajouts des headers

---
+++ interface.h
@@ -0,0 +1,16 @@
+//#ifndef INTERFACE_H
+
+#define INTERFACE_H
+
+
+extern const int rouge;
+extern const int orange;
+extern const int jaune;
+extern const int vert;
+extern const int bleu;
+extern const int violet; 
+
+
+void affiche_grille(int grille[nblignes][nbcolonnes],WINDOW *fenetre);
+
+void initialisation_interface(int grille[nblignes][nbcolonnes], WINDOW *fenetre);
---
+++ regles.h
@@ -0,0 +1,7 @@
+//#ifndef REGLES_H
+#define REGLES_H
+
+extern const int nblignes;
+extern const int nbcolonnes;
+
+bool endgame(int* grille);
